a0adbe47e8882a230d72161278f315f539add6af
Sau P <sau@yaaama.xyz>
2024-09-04 12:39:55 +0100

    Add some c flags

--- config.mk
+++ config.mk
@@ -21,6 +21,7 @@ LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft -lXrender \
        `$(PKG_CONFIG) --libs freetype2`
 
 # flags
+CFLAGS=-O2 -march=native -mtune=native
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
 STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
 STLDFLAGS = $(LIBS) $(LDFLAGS)
@@ -33,4 +34,4 @@ STLDFLAGS = $(LIBS) $(LDFLAGS)
 #MANPREFIX = ${PREFIX}/man
 
 # compiler and linker
-# CC = c99
+CC = gcc
